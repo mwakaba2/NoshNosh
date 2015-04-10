5c02fd1548ac70d0451ca09950d3074c793215d8
Mariko Wakabayashi <marimarimekko@gmail.com>
2015-04-10 04:13:38 -0500

    Edited Readme

---
+++ NoshNosh/DrinkDishTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  DrinkDishTableCellTableViewCell.swift
+//  NoshNosh
+//
+//  Created by Mariko Wakabayashi on 4/10/15.
+//  Copyright (c) 2015 wakawaka. All rights reserved.
+//
+
+import UIKit
+
+class DrinkDishTableCell: UITableViewCell {
+
+
+    @IBOutlet weak var imgItem: UIImageView!
+
+    @IBOutlet weak var nameItem: UILabel!
+    
+    @IBOutlet weak var restaurantName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NoshNosh
+
+
+Week One 
+
+- Completed Layout for LandingPage, Events/Specials, Restaurants, Drinks/Dishes, Surprise Me
+
+
+Week Two Goal
+
+- Gather data from Noshfolio API and implement data models
